c1be10b8ca92ceff8abc93ddadbdf0412d4da501
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-10-21 20:44:52 +0500

    degree into radian

---
+++ hack2.c
@@ -0,0 +1,50 @@
+/*
+    Author: Eman Syed
+    Date: 21/10/2023
+
+
+*/
+#include<stdio.h>
+#include<math.h>
+//globally decleare variable 
+ double lon1,p1,lon2,p2,delta,Radius = 62371,Result=0;
+//Function to convert Degree into Radian
+int Degree_to_Radian(int num)
+{
+    float PI = 3.1416f;
+    int radian = (num/180)*PI;
+
+    return radian;
+}
+
+int main()
+{
+   
+
+    printf("Enter the longitude of Origin in Degree [ -180 to 180 ] : ");
+    scanf("%lf",&lon1);
+    printf("Enter the latitude of Origin in Degree [ -90 to 90 ] : ");
+    scanf("%lf",&p1);
+    printf("Enter the longitude of Distination in Degree [ -180 to 180 ] : ");
+    scanf("%lf",&lon2);
+    printf("Enter the latitude of Distination in Degree [ -90 to 90 ] : ");
+    scanf("%lf",&p2); 
+    delta = lon1 - lon2;    
+
+    printf("Location Distance \n");
+    printf("===================\n");
+    printf("Origin : %lf %lf \n",lon1,p1);
+    printf("Distination : %lf %lf \n",lon2,p2);
+
+    //calling Degree_to_Radian Function 
+    lon1 = Degree_to_Radian(lon1);
+    p1 = Degree_to_Radian(p1);
+    lon2 = Degree_to_Radian(lon2);
+    p2 = Degree_to_Radian(p2);
+
+
+    Result = acos((sin(p1) * sin(p2) + cos(p1) * cos(p2) * cos(delta)))*Radius;
+    printf("Air distance is %lf kms \n",Result);
+
+    return 0;
+}
